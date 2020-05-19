7af498c50f595c123c903853fb3fed7809719e55
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-05-19 10:26:40 -0400

    this is my updated JS with moment and styling

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// General wiki search AJAX call
-
-// Hiding and displaying main content boxes upon search
 $("form").on("submit", function (event) {
   event.preventDefault();
-
+  // everything for the ticketmaster API
   var genre = $("input[name='answer']:checked").val();
   var searchCity = $("#cityInput").val().trim();
   var concertURL =
@@ -19,65 +16,98 @@ $("form").on("submit", function (event) {
     console.log(concerts);
     $("#welcomeInfo").addClass("is-hidden");
     $("#artistShows").removeClass("is-hidden");
-    for (var i = 0; i < 3; i++) {
-      var eventName = $("<h1>").text(concerts._embedded.events[i].name);
-      eventName.css({
-        "font-size": "30px",
-        "text-allign": "center",
-      });
-      $("#artistShows").append(eventName);
 
-      var genre = $("<p>").text(
-        "Genre: " + concerts._embedded.events[i].classifications[0].genre.name
+    // if statement to deliver a message if there are no scheduled concerts for a specific ciy / genre. this has if statement is wrapped around the for loop
+    if (concerts.page.totalPages !== 0) {
+      // for loop, limited to 3, we found that this is a happy medium due to shows being cancelled from Covid 19.
+      for (let i = 0; i < 3; i++) {
+        var eventName = $("<h1 class='show-title'>").text(
+          concerts._embedded.events[i].name
+        );
+        eventName.css({
+          "font-size": "30px",
+          "text-allign": "center",
+        });
+        $("#artistShows").append(eventName);
+
+        var genre = $("<p class='show-info'>").text(
+          "Genre: " + concerts._embedded.events[i].classifications[0].genre.name
+        );
+        $("#artistShows").append(genre);
+
+        var artistName = $("<p class='show-info'>").text(
+          "Artist Name: " +
+            concerts._embedded.events[i]._embedded.attractions[0].name
+        );
+        $("#artistShows").append(artistName);
+
+        var StartDate = $("<p class='show-info'>").text(
+          "Date: " + concerts._embedded.events[i].dates.start.localDate
+        );
+        $("#artistShows").append(StartDate);
+
+        // military time to be displayed in AM / PM time - using Moment.js to mash the two API's together
+        var time = moment(
+          concerts._embedded.events[i].dates.start.localTime,
+          "HH:mm:"
+        ).format("h:mm A");
+        var startTime = $("<p class='show-info'>").text("Time: " + time);
+        $("#artistShows").append(startTime);
+
+        // if statements for price range, to give a message if the prices are not available
+        if (!concerts._embedded.events[i].priceRanges) {
+          var priceHigh = $("<p class='show-info'>").text("High Price: N/A");
+          var priceLow = $("<p class='show-info'>").text("Low Price: N/A");
+        } else {
+          var priceHigh = $("<p class='show-info'>").text(
+            "High Price: $" + concerts._embedded.events[i].priceRanges[0].max
+          );
+
+          var priceLow = $("<p class='show-info'>").text(
+            "Low Price: $" + concerts._embedded.events[i].priceRanges[0].min
+          );
+        }
+        $("#artistShows").append(priceHigh);
+        $("#artistShows").append(priceLow);
+
+        var venueName = $("<p class='show-info'>").text(
+          "Venue: " + concerts._embedded.events[i]._embedded.venues[0].name
+        );
+        $("#artistShows").append(venueName);
+
+        var venueAddress = $("<p class='show-info'>").text(
+          "Address: " +
+            concerts._embedded.events[i]._embedded.venues[0].address.line1
+        );
+        $("#artistShows").append(venueAddress);
+
+        // adding a button click to link to ticketmaster's website, this is a requirement in their documentaiton for using their API, plus this shows the responsive nature of the applicaiton.
+        var newLink = $(
+          '<button class="button is-info is-inverted is-outlined">'
+        )
+          .click(function () {
+            var ticketLink = concerts._embedded.events[i].url;
+            window.open(ticketLink, "_blank");
+          })
+          .text("Click here to buy a ticket!");
+        $("#artistShows").append(newLink);
+
+        $("#artistShows").append("<hr>");
+      }
+    } else {
+      // user will be promped with this message, telling them that the entered criteria did not return any results
+      var noResults = $("<p class='show-info'>").text(
+        "There are no events currently scheduled under the genre: " +
+          $("input[name='answer']:checked").val() +
+          " " +
+          "in " +
+          searchCity +
+          " at this time, Sorry! Please check back at a later date, due to the COVID 19 pandemic, many events are being rescheduled."
       );
-      $("#artistShows").append(genre);
-
-      var artistName = $("<p>").text(
-        "Artist Name: " +
-          concerts._embedded.events[i]._embedded.attractions[0].name
-      );
-      $("#artistShows").append(artistName);
-
-      var StartDate = $("<p>").text(
-        "Date: " + concerts._embedded.events[i].dates.start.localDate
-      );
-      $("#artistShows").append(StartDate);
-
-      var StartTime = $("<p>").text(
-        "Time: " + concerts._embedded.events[i].dates.start.localTime
-      );
-      $("#artistShows").append(StartTime);
-
-      // var priceHigh = $("<p>").text("Price (high): " + concerts._embedded.events[i].priceRanges[0].max || "this informaiton is not available at the time")
-      // $("#artistShows").append(priceHigh);
-
-      // var priceLow = $("<p>").text("Price (low): " + concerts._embedded.events[i].priceRanges[0].min || "this information is not available at the time")
-      // $("#artistShows").append(priceLow);
-
-      var venueName = $("<p>").text(
-        "Venue: " + concerts._embedded.events[i]._embedded.venues[0].name
-      );
-      $("#artistShows").append(venueName);
-
-      var venueAddress = $("<p>").text(
-        "Address: " +
-          concerts._embedded.events[i]._embedded.venues[0].address.line1
-      );
-      $("#artistShows").append(venueAddress);
-
-      var ticketLink = concerts._embedded.events[i].url;
-      var newLink = $('<button class="button is-info is-inverted is-outlined">')
-        .click(function () {
-          window.location = "" + ticketLink + "";
-          window.EventTarget = "_blank";
-        })
-        .text("Click here to buy a ticket!");
-
-      $("#artistShows").append(newLink);
-      $("#artistShows").append("<hr>");
     }
-    // make the div show up, and others dissapear
+    $("#artistShows").append(noResults);
 
+    // make the welcome page dissapear, and show section appear upon form submit.
     $("#welcomeInfo").addClass("is-hidden");
     $("#artistShows").removeClass("is-hidden");
   });
@@ -146,6 +176,7 @@ $("form").on("submit", function (event) {
   });
 });
 
-var jsonUri = "data:text/plain;base64," + window.btoa(JSON.stringify(particleSettings));
+var jsonUri =
+  "data:text/plain;base64," + window.btoa(JSON.stringify(particleSettings));
 console.log(jsonUri);
 particlesJS.load("particles-js", jsonUri);
